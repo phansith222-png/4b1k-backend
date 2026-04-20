1f2cd3a264f0560a568283a959577101f05e67a4
Patipatpatlom <patipatpatlom@gmail.com>
2026-04-20 17:25:52 +0700

    Update Backend

--- src/middlewares/authenticate.middleware.js
+++ src/middlewares/authenticate.middleware.js
@@ -21,20 +21,6 @@ export default async function authenicateMiddleware(req, res, next) {
     return next(createHttpError[401]("Unauthorized 2: Token is missing"));
   }
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    const foundUser = await getUserby("id", payload.id);
-
-    if (!foundUser) {
-      return next(createHttpError[401]("Unauthorized 3: User not found"));
-    }
-
-    const { createdAt, updatedAt, ...userInfo } = foundUser;
-
-    if (!token) {
-        return next(createHttpError[401]('Unauthorized 2: Token is missing'))
-    }
-
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         const foundUser = await getUserby('id', payload.id)
